734bdace8534186aaa04265c082b6a42e4a055d3
Brian Kim <bhk3164@gmail.com>
2023-07-28 23:30:11 -0700

    Worked on api query

--- src/App.js
+++ src/App.js
@@ -4,13 +4,30 @@ import { useState } from 'react';
 
 function App() {
   const [id, setID] = useState("");
+  const [data, setData] = useState("");
 
   function handleChange(event) {
     setID(event.target.value);
   }
   function clickButton(event) {
     event.preventDefault();
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://lichess.org/api/broadcast/${id}.pgn`, {
+          headers: {
+            accept: 'application/x-ndjson'
+          }
+        });
+        const responseData = await response.json();
+        setData(responseData);
 
+      } catch (error) {
+        console.log('Error fetching data: ', error);
+      }
+    };
+    if (fetchData) {
+      console.log(data);
+    }
 
   }
   return (
